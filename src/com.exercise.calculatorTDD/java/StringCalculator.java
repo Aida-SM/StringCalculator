964ba55cd12030fdc10c37c20099039ea095a72b
Aida Sabaghmoghadam <aida.sabbagh@gmail.com>
2019-02-17 20:47:31 -0500

    refactor: deleting some white spaces

--- src/com.exercise.calculatorTDD/java/StringCalculator.java
+++ src/com.exercise.calculatorTDD/java/StringCalculator.java
@@ -4,12 +4,10 @@ public class StringCalculator {
 
     private int sum = 0;
 
-
     public  String add(String textAsNumber) {
         if (textAsNumber.isEmpty())
             return "0";
         else if(textAsNumber.contains("\n,")|| textAsNumber.contains(",\n")){
-
             return "Wrong input";
         }
         else if (textAsNumber.contains(",")) {
@@ -19,19 +17,14 @@ public class StringCalculator {
             }
             return String.valueOf(sum);
         }
-
         else if(textAsNumber.contains("\n")){
              String[] splitNumbers = textAsNumber.split("\n");
             for (String split: splitNumbers) {
                 sum += Integer.parseInt(split) ;
             }
             return String.valueOf(sum);
-
         }
-
         else
             return textAsNumber ;
-
     }
-
 }
